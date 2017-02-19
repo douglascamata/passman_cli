5ca02bb55bd1d548f1120b3eee38d0c0158707b5
Douglas Camata <d.camata@gmail.com>
2017-02-19 01:33:21 +0100

    Some pep8 adjustments

--- passman_cli/cli/credential.py
+++ passman_cli/cli/credential.py
@@ -9,15 +9,15 @@ def entry_point(ctx):
 
 @entry_point.command("new")
 @click.option("--data", "-d", required=True,
-             help="JSON used to create a credential" \
-                  "(check https://github.com/nextcloud/passman/wiki/API)")
+              help="JSON used to create a credential"
+                   "(check https://github.com/nextcloud/passman/wiki/API)")
 def new_credential(ctx):
     print ctx.obj["passman"].new_credential(json.loads(data))
 
 
 @entry_point.command("update")
 @click.option("--data", "-d", required=True,
-              help="JSON used to update a credential " \
+              help="JSON used to update a credential "
                    "(check https://github.com/nextcloud/passman/wiki/API)")
 def update_credential(ctx):
     print ctx.obj["passman"].update_credential(json.loads(data))
